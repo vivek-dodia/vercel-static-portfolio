6be9cd1e389887c16084677a9832ed18d5c4c0f8
vivek-dodia <vivek.dodia@icloud.com>
2024-10-30 00:57:31 -0400

    update contact page color

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,15 +5,12 @@ import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
-	
-{
-    icon: <Linkedin size={20} />,
-    href: "https://www.Linkedin.com/in/vivekdodia/",
-    label: "Linkedin",
-    handle: "@vivekdodia",
-},
-
-
+	{
+		icon: <Linkedin size={20} />,
+		href: "https://www.Linkedin.com/in/vivekdodia/",
+		label: "Linkedin",
+		handle: "@vivekdodia",
+	},
 	{
 		icon: <Mail size={20} />,
 		href: "mailto:vivek.dodia@icloud.com",
@@ -30,16 +27,16 @@ const socials = [
 
 export default function Example() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className="bg-gradient-to-tl from-pink-200 to-yellow-200"> {/* Updated gradient background */}
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
 				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
 					{socials.map((s) => (
-						<Card>
+						<Card key={s.label}> {/* Added key prop for Card */}
 							<Link
 								href={s.href}
 								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16"
 							>
 								<span
 									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
@@ -47,7 +44,7 @@ export default function Example() {
 								/>
 								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
 									{s.icon}
-								</span>{" "}
+								</span>
 								<div className="z-10 flex flex-col items-center">
 									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
 										{s.handle}
