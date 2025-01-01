7765d7ca314d359b0707bdd9605e8a7495419449
vivek-dodia <vivek.dodia@icloud.com>
2025-01-01 18:00:23 -0500

    update color scheme

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,14 @@ const navigation = [
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-pink-200 to-yellow-200">
+		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black to-blue-800">
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex flex-wrap items-center justify-center gap-4">
 					{navigation.map((item) => (
 						<Link
 							key={item.href}
 							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+							className="text-sm duration-500 text-transparent bg-white bg-clip-text hover:text-transparent hover:bg-white"
 						>
 							{item.name}
 						</Link>
@@ -35,13 +35,13 @@ export default function Home() {
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 mx-6">
+				<h2 className="text-sm text-transparent bg-white bg-clip-text mx-6">
 					Hi, I'm Vivek, automating networks and infrastructure at
 					{" "}
 					<Link
 						target="_blank"
 						href="https://www.resibridge.com/about-us"
-						className="underline duration-500 hover:text-zinc-300"
+						className="underline duration-500 text-transparent bg-white bg-clip-text hover:text-transparent hover:bg-white"
 					>
 						ResiBridge
 					</Link>
@@ -51,7 +51,7 @@ export default function Home() {
 					<Link
 						target="_blank"
 						href="https://github.com/vivek-dodia"
-						className="underline duration-500 hover:text-zinc-300"
+						className="underline duration-500 text-transparent bg-white bg-clip-text hover:text-transparent hover:bg-white"
 					>
 						Git
 					</Link>{" "}
